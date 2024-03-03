8b0a56a3274170e2d81e58df2856e8c5fae71b0a
Sakina Naqvi <sakinana@usc.edu>
2024-03-02 23:23:03 -0800

    FINAL

--- stack.h
+++ stack.h
@@ -6,7 +6,7 @@
 
 // Use inheritance from std::vector (choose public/private) as appropriate
 template <typename T>
-class Stack 
+class Stack : private std::vector<T>
 {
 public:
     Stack();
@@ -17,7 +17,52 @@ public:
     void pop();  // throws std::underflow_error if empty
     const T& top() const; // throws std::underflow_error if empty
     // Add other members only if necessary
+
+    private: 
+        int topVal;
 };
+
+template <typename T>
+Stack<T>::Stack() {
+  topVal = -1;
+  
+}
+template <typename T>
+Stack<T>::~Stack() {
+
+}
+template <typename T>
+bool Stack<T>::empty() const {
+ if(topVal == -1) 
+  return 1;
+ else 
+    return 0;
+}
+template <typename T>
+void Stack<T>::push(const T& item) {
+  topVal += 1;
+  std::vector<T>::push_back(item);
+}
+template <typename T>
+void Stack<T>::pop() {
+  if(empty()) {
+    throw std::underflow_error("Already Empty");
+  } else  {
+      std::vector<T>::pop_back();
+      topVal--;
+  }
+}
+template <typename T>
+const T& Stack<T>::top() const {
+  if(empty())
+    throw std::underflow_error("Already Empty");
+   else 
+      return std::vector<T>::back();;
+}
+template <typename T>
+size_t Stack<T>::size() const {
+    return std::vector<T>::size();
+}
 
 
 #endif
