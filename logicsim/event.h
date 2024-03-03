fc9456e0ba467e6ede77e6c9bda59a999fde2811
Sakina Naqvi <sakinana@usc.edu>
2024-03-02 23:24:38 -0800

    FINAL

--- logicsim/event.h
+++ logicsim/event.h
@@ -11,6 +11,10 @@ struct Event
 
 typedef struct EventLess {
         //write the operator() required to make this a functor that compares Events by time
+        bool operator()(Event* firstEvent, Event* secondEvent) {
+            if(secondEvent->time > firstEvent->time) return true;
+            else return false;
+        }
 } EventLess;
 	
 #endif
