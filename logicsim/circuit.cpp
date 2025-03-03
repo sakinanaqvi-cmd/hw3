5f21b16ce635c195a36d8256c7a3b2ee4eaf61b2
Sakina Naqvi <sakinana@usc.edu>
2025-03-02 17:05:34 -0800

    updated circuit

--- logicsim/circuit.cpp
+++ logicsim/circuit.cpp
@@ -110,6 +110,15 @@ bool Circuit::parse(const char* fname)
                     m_gates.push_back(new Or2Gate(m_wires[stoi(s_in1)], m_wires[stoi(s_in2)], m_wires[stoi(s_output)]));
                 }
                 //Add code here to support the NOT gate type
+                if(s_type == "NOT")
+                {
+                    std::string s_in1;
+                    getline(ss, s_in1, ',');
+                   
+                    std::string s_output;
+                    getline(ss, s_output, ',');
+                    m_gates.push_back(new NotGate(m_wires[stoi(s_in1)], m_wires[stoi(s_output)]));
+                }
             }
         }
         if(line == "INJECT")
@@ -197,19 +206,19 @@ void Circuit::startUml(std::ostream& os)
     for(auto w : m_wires)
     {
         if(w->getName().size() > 0)
-				{
-					os << "binary " << "\"" << w->getName() << "\"" << " as W" << w->getId() << std::endl;
-				}
-		}
+	{
+		os << "binary " << "\"" << w->getName() << "\"" << " as W" << w->getId() << std::endl;
+	}
+     }
     os << std::endl;
     os << "@0" << std::endl;
     for(auto w : m_wires)
     {
         if(w->getName().size() > 0)
-				{
-					os << "W" << w->getId() << " is {low,high} " << std::endl;
-				}
-		}
+	{
+		os << "W" << w->getId() << " is {low,high} " << std::endl;
+	}
+    }
     os << std::endl;
     
 }
